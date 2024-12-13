945f751ce4bca80de2a55be388a01b77e6948e52
Unho Chang <unho.chang@gmail.com>
2024-12-13 10:12:12 +0900

    mdis 이동인구 체크 코드 정리 커밋

---
+++ nia2_07_모델링_유형화_데이터.sql
@@ -0,0 +1,30 @@
+select * 
+from 김재린_월간_최종가공_v4
+limit 100;
+
+select count(*) 
+from 김재린_월간_최종가공_v4;
+
+select * 
+from 김재린_월간_최종가공_v4;
+
+select count(*) 
+from 김재린_주간_최종가공_v4;
+
+select * 
+from 김재린_주간_최종가공_v4;
+
+select * 
+from 모델_데이터_시군구_연간;
+
+select * 
+from 지역특성유형화;
+
+select * 
+from 행안부_지방재정_연간_부문별_정제;
+
+select count(*) 
+from 지방재정365_시군구_주간_정제;
+
+select * 
+from 지방재정365_시군구_주간_정제;
